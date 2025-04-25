ad5dbbe23439d8deef133c346a9adce671765d6d
KondkariYusuf <kondkariyusuf345@gmail.com>
2025-04-25 19:09:25 +0530

    Add disk allocation and deallocation code

--- contagious.py
+++ contagious.py
@@ -1,26 +1,16 @@
-# Disk with 20 blocks
-disk = [None] * 20
+disk = [None] * 20  # 20 blocks
+files = [("file1", 2, 5), ("file2", 8, 4), ("file3", 17, 4)]
 
-# Allocate file1 from block 2 to 6 (5 blocks)
-for i in range(2, 7):
-    disk[i] = "file1"
+for name, start, length in files:
+    if start + length <= len(disk) and all(disk[i] is None for i in range(start, start + length)):
+        for i in range(start, start + length):
+            disk[i] = name
+        print(f"{name} allocated from {start} to {start + length - 1}")
+    else:
+        print(f"{name} not allocated")
 
-# Allocate file2 from block 8 to 11 (4 blocks)
-for i in range(8, 12):
-    if disk[i] is None:
-        disk[i] = "file2"
+# Show final disk status
+print("\nDisk status:")
+for i in range(len(disk)):
+    print(f"Block {i}: {disk[i] if disk[i] else 'Free'}")
 
-# Show disk status
-print("Disk blocks:")
-for i in range(20):
-    print(i, ":", disk[i] if disk[i] else "Free")
-
-# Deallocate file1
-for i in range(20):
-    if disk[i] == "file1":
-        disk[i] = None
-
-# Show disk after deallocation
-print("\nAfter deleting file1:")
-for i in range(20):
-    print(i, ":", disk[i] if disk[i] else "Free")
